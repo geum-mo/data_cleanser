b820d7712a89534c957dabfe3fb534df0e952694
Geummo Kang <kang.keum.mo@gmail.com>
2019-12-08 16:49:45 +0900

    commit

--- main.py
+++ main.py
@@ -8,20 +8,53 @@ path = "/Users/geummokang/Documents/GitHub/data_cleanser/dataset/rawDataset.csv"
 
 df = pd.read_csv(path, header=None)
 
+''' Read the following article to grasp the basic of pandas '''
+''' https://www.shanelynn.ie/using-pandas-dataframe-creating-editing-viewing-data-in-python/ '''
+
+
 ''' Indexing columns '''
 # print(df.columns)
 # print(df[0])
 # print(df[1])
 
+''' Search indexes with empty values (NaN)'''
+
+# 1. for spec
+print(len(df[0]))
+blank_rows = np.where(pd.isnull(df))[0]  # Return an array of rows
+print(len(blank_rows))
+# print(np.where(pd.isnull(df))[1]) # Return an array of columns
+print(len(df.drop(df.blank_rows, axis=0)))
+
+
+''' Print a specific or a range of rows '''
+
+# 1. A specific row:
+#   print(df.loc[22562])
+# 2. A range of row:
+#   print(df.loc[22562:22566])
+
+''' Print a specific value or a range of value '''
+
+# 1. A specific value:
+#   print(df[0].loc[22562])
+# 2. A range of value:
+#   print(df[0].loc[22562:22566])
+
 ''' Look up for a row containing specific strings in its value '''
-# print(df[0].str.contains('"""')) # You need to index a column.
-# print(df[1].str.contains('"""')) # It returns Series of Boolean.
+# print(df[0].str.contains('"""').value_counts())  # You need to index a column.
+# It returns Series of Boolean.
+# print(df[1].str.contains('"""').value_counts())
 
-'''
+# print(df[0].str.contains('"""').value_counts().loc[False])
 
-''' Replacing multiple 's to a single '.'''
+# print(df[1].str.contains('"""').value_counts().loc[False])
 
-df.replace("'''", "'")
-df.replace("''", "'")
+# df[0].replace('누구세요', "누구야")
+# df[1].replace('Who is it', "Who's it")
 
-print(df[0].str.contains('"""'))
+# print(df[0])
+# print(df[1])
+
+# print(df[0].str.contains('누구세요'))  # You need to index a column.
+# print(df[1].str.contains('Who is it'))  # It returns Series of Boolean.
