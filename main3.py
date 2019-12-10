7d5c26fd325638c9ded2ac5b9a710085dc03ab37
Geummo Kang <kang.keum.mo@gmail.com>
2019-12-10 22:53:47 +0900

    commit

--- main3.py
+++ main3.py
@@ -11,7 +11,7 @@ path = "./dataset/inProg_main2.csv"
 df = pd.read_csv(path, header=None)
 # print(df)
 print(f"{len(df)} << Total length of in-progress(main) data")
-
+# print(df)
 """ Naming columns """
 
 df.columns = ["ko", "en"]
@@ -19,39 +19,48 @@ df.columns = ["ko", "en"]
 
 """ Looking up rows containing -s """
 
+"""
 lst_bool = df["ko"].str.contains("-") | df["ko"].str.contains("-")
 index = df["ko"].index[lst_bool]
 df.loc[index, :].to_csv("./dataset/noMatch.csv", index=False, header=None)
+"""
 
 
 """ Removing leading & trailing whitespaces """
 
-df["ko"] = df["ko"].str.strip
-df["en"] = df["en"].str.strip
+df["ko"] = df["ko"].str.strip()
+df["en"] = df["en"].str.strip()
 
 
 """ Removing empty square brackets, if any """
 
+"""
 cond = df["ko"].str.contains("\[]") | df["en"].str.contains("\[]")
 print(df.loc[cond, :])
 df = df.drop(df["ko"].index[cond])
 print(len(df))
+"""
 
+""" []s """
 
-
+# print(df)
+cond = (df["ko"].str.count("\[") > 1) | (df["en"].str.count("\[") > 1)
+print(df.loc[cond])
+print(len(df.loc[cond]))
+df = df.drop(df["ko"].index[cond])
+print(len(df))
 
 
 """ Look up and remove duplicated rows """
 
-"""
+
 # Look up duplicates, it returns a Boolean Series
-duplicates = df.duplicated([0], keep="first")
+duplicates = df.duplicated("ko", keep="first")
 print(len(duplicates))
 
 # Returns Dataframe of True value
-print(df[df.duplicated([0], keep="first")])
+print(df[df.duplicated("ko", keep="first")])
 
 # Remove duplicates
-duplicates_removed = df.drop(df.index[duplicates], axis=0)
-print(len(duplicates_removed))
-"""
+df = df.drop(df.index[duplicates], axis=0)
+print(len(df))
