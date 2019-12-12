7beea2308ffad3788bfb468fa39c74589645d2c6
Geummo Kang <kang.keum.mo@gmail.com>
2019-12-12 11:08:31 +0900

    commit

--- main4.py
+++ main4.py
@@ -55,21 +55,44 @@ condB1 = df["ko"].str.len() | df["en"].str.len() >= 80
 
 s = df.loc[condA & condB]
 ns = df.loc[condA1 | condB1]
-print(ns)
 
-print(s)
-TRAIN1 = ns
-TRAIN2 = s.sample(n=1330748, random_state=1)
-VALIDATION = s.sample(n=10000, random_state=2)
-TEST = s.sample(n=10000, random_state=3)
+samples = s.sample(n=19998, random_state=1)
+
+samples = s.sample(n=19998)
+VAL = samples.sample(n=9999)
+TEST = samples.drop(VAL.index, axis=0)
+TRAIN = df.drop(samples.index, axis=0)
+
+print(VAL)
+print(TEST)
+print(TRAIN)
+
+
+""" 
+for e in VAL.index:
+    if e in (TEST.index) | (TRAIN.index):
+        print(f'{e} is duplicated')
+    else:
+        print(f'{e} in validation file is not duplicated')
+        pass
+for e2 in TEST.index:
+    if e2 in (VAL.index) | (TRAIN.index):
+        print(f'{e2} is duplicated')
+    else: 
+        print(f'{e2} in test file is not duplicated')
+        pass
+for e3 in TRAIN.index:
+    if e3 in (VAL.index) | (TEST.index):
+        print(f'{e3} is duplicated')
+    else:
+        print(f'{e3} in train file is not duplicated')
+        pass
+""" 
 
 savePath1 = "./dataset/validation.tsv"
 savePath2 = "./dataset/test.tsv"
-savePath3 = "./dataset/train1.tsv"
-savePath4 = "./dataset/train2.tsv"
+savePath3 = "./dataset/train.tsv"
 
-VALIDATION.to_csv(savePath1, sep="\t", index=False, header={"ko","en"})
+VAL.to_csv(savePath1, sep="\t", index=False, header={"ko","en"})
 TEST.to_csv(savePath2, sep="\t", index=False, header={"ko","en"})
-TRAIN1.to_csv(savePath3, sep="\t", index=False, header={"ko","en"})
-TRAIN2.to_csv(savePath4, sep="\t", index=False, header={"ko","en"})
-
+TRAIN.to_csv(savePath3, sep="\t", index=False, header={"ko","en"})
--- test.py
+++ test.py
@@ -18,100 +18,29 @@ df = pd.DataFrame(
     dtype="category",
 )
 
-df.columns = ["ko", "en"]
 print(df)
-# counts = df["A"].str.count("-")
 
-noMatch = pd.DataFrame(df["A"].str.count("-") != df["B"].str.count("-"))
-# print(noMatch)
-# print(len(noMatch[0].index[noMatch[0] == True]))
-# df = df.drop(df.index[noMatch[0].index[noMatch[0] == True]], axis=0)
-# print(df)
-df.index = range(len(df))
-# (df["A"].str.contains("- ")
-# (df["B"].str.contains("- ")
+s = df.sample(n=3)
+s1 = s.sample (n=1)
+s2 = s.drop(s1.index, axis=0)
+print(s1)
+print(s2)
 
-# print(df)
-listA = df["A"].str.count("-")
-listB = df["B"].str.count("-")
+r = df.drop(s.index, axis=0)
 
-# print(df.iloc[np.where(listA | listB > 2)])
-# print(len(df.iloc[np.where(listA | listB > 2)]))
-
-# df = df.drop(df.index[np.where(listA | listB > 2)], axis=0)
+print(r)
 
 
-df = df.replace(to_replace="^-|^\s", value="", regex=True)
-# print(df)
-
-# print(df.explode(df["A"].str.split(pat="-")))
-
-df["A"] = df["A"].str.split(pat="\s-|-\s|\s-\s")
-df["B"] = df["B"].str.split(pat="\s-|-\s|\s-\s")
-
-# print(df)
-
-# print(pd.DataFrame({col: pd.concat(df[col]) for col in df.columns}))
-
-
-"""
-def explode(df, lst_cols, fill_value=""):
-    # make sure `lst_cols` is a list
-    if lst_cols and not isinstance(lst_cols, list):
-        lst_cols = [lst_cols]
-    # all columns except `lst_cols`
-    idx_cols = df.columns.difference(lst_cols)
-
-    # calculate lengths of lists
-    lens = df[lst_cols[0]].str.len()
-
-    if (lens > 0).all():
-        # ALL lists in cells aren't empty
-        return (
-            pd.DataFrame(
-                {
-                    col: np.repeat(df[col].values, df[lst_cols[0]].str.len())
-                    for col in idx_cols
-                }
-            )
-            .assign(**{col: np.concatenate(df[col].values) for col in lst_cols})
-            .loc[:, df.columns]
-        )
+for e in s1.index:
+    if e in (s2.index) | (r.index):
+        print(f'{e} is duplicated')
     else:
-        # at least one list in cells is empty
-        return (
-            pd.DataFrame(
-                {
-                    col: np.repeat(df[col].values, df[lst_cols[0]].str.len())
-                    for col in idx_cols
-                }
-            )
-            .assign(**{col: np.concatenate(df[col].values) for col in lst_cols})
-            .append(df.loc[lens == 0, idx_cols])
-            .fillna(fill_value)
-            .loc[:, df.columns]
-        )
-
-
-print(explode(df, lst_cols=[0, 1]))
-"""
-
-# df = df.drop(df.index[["A"].str.count("-") > 2], axis=0)
-
-"""
-if listA[0] | listB[0] > 0:
-    print(
-        df[
-            (df["A"].str.count("-") != df["A"].str.count("- "))
-            | (df["B"].str.count("-") != df["B"].str.count("- "))
-        ]
-    )
-"""
-
-# def noWhiteSpaceRight(column)
-
-
-# def noWhiteSpaceLeft(column)
-
-# df = df.drop(df.index[blank_rows], axis=0)  # Removes the blank rows
-
+        for e2 in s2.index:
+            if e2 in (s1.index) | (r.index):
+                print(f'{e2} is duplicated')
+            else: 
+                for e3 in r.index:
+                    if e3 in (s1.index) | (s2.index):
+                        print(f'{e3} is duplicated')
+                    else:
+                        pass
